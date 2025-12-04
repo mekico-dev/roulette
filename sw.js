30e7f7cacf60de248f1fb9c4b09baa51c1dbbc3d
mekico-dev <bay.mekico@gmail.com>
2025-12-05 00:34:30 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'moraeru-cache-v1';
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './bg.png',
+  './flower.png',
+  './hatgirl.png',
+  './purple.png',
+  './keyholder.png',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  const request = event.request;
+  if (request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(request).then(cached => {
+      if (cached) return cached;
+      return fetch(request).catch(() => cached);
+    })
+  );
+});
